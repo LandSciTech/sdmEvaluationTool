d5136503e87528b13fbca62bee51dc73d6a77aff
Peter Solymos <psolymos@gmail.com>
2025-11-02 14:12:11 -0700

    Fixing db functions
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

---
+++ working/working-with-bam-db.R
@@ -0,0 +1,25 @@
+# read tables
+
+devtools::load_all("sdmEvalToolCore")
+sdmevaltool_options(base = "./misc/base") # use the misc folder
+
+lang <- "english"
+userid <- "draper"
+deploymentid <- "deployment1"
+modelid <- "bam_v5_can71"
+
+con <- db_connect()
+DBI::dbListTables(con)
+
+# note: components should be pulled from the package
+comps <- sdmEvalToolCore::components
+
+userinfo <- db_read_user_info(con, userid, deploymentid)
+attr(userinfo, "user_roles")
+
+dm <- db_read_deployment_materials(con, deploymentid)
+
+comms <- db_read_comments(con, deploymentid)
+evals <- db_read_evaluations(con, deploymentid)
+
+DBI::dbDisconnect(con)
