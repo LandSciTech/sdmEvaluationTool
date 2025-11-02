a593bf1ae494e382cac748d6ec3d45f5ab954185
Peter Solymos <psolymos@gmail.com>
2025-11-01 19:22:08 -0600

    Reformat
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- RELEASE.R
+++ RELEASE.R
@@ -5,23 +5,39 @@ pkg <- "sdmEvalToolCore"
 conf <- yaml::read_yaml("spec/config.yml")
 
 user_roles <- do.call(rbind, lapply(conf$roles, as.data.frame))
-save(user_roles, file = file.path(pkg, "data", "user_roles.rda"), compress="xz")
+save(
+    user_roles,
+    file = file.path(pkg, "data", "user_roles.rda"),
+    compress = "xz"
+)
 
-tables <- do.call(rbind, lapply(names(conf$tables), \(i) {
-    l <- conf$tables[[i]]
-    data.frame(table=i, name=l$name, description=l$description)
-}))
-save(tables, file = file.path(pkg, "data", "tables.rda"), compress="xz")
+tables <- do.call(
+    rbind,
+    lapply(names(conf$tables), \(i) {
+        l <- conf$tables[[i]]
+        data.frame(table = i, name = l$name, description = l$description)
+    })
+)
+save(tables, file = file.path(pkg, "data", "tables.rda"), compress = "xz")
 
-fields <- do.call(rbind, lapply(names(conf$tables), \(i) {
-    l <- conf$tables[[i]]$fields
-    data.frame(table=i, do.call(rbind, lapply(names(l), \(j) {
-        k <- l[[j]]
-        k[sapply(k, is.null)] <- NA_character_
-        data.frame(field=j, as.data.frame(k))
-    })))
-}))
-save(fields, file = file.path(pkg, "data", "fields.rda"), compress="xz")
+fields <- do.call(
+    rbind,
+    lapply(names(conf$tables), \(i) {
+        l <- conf$tables[[i]]$fields
+        data.frame(
+            table = i,
+            do.call(
+                rbind,
+                lapply(names(l), \(j) {
+                    k <- l[[j]]
+                    k[sapply(k, is.null)] <- NA_character_
+                    data.frame(field = j, as.data.frame(k))
+                })
+            )
+        )
+    })
+)
+save(fields, file = file.path(pkg, "data", "fields.rda"), compress = "xz")
 
 cmp <- conf$components
 components <- data.frame(
@@ -31,7 +47,8 @@ components <- data.frame(
     upload = character(0L),
     display = character(0L),
     evaluation = character(0L),
-    reporting = character(0L))
+    reporting = character(0L)
+)
 for (i in names(cmp)) {
     l <- cmp[[i]]
     c1 <- data.frame(
@@ -42,14 +59,20 @@ for (i in names(cmp)) {
         upload = NA_character_,
         display = NA_character_,
         evaluation = NA_character_,
-        reporting = NA_character_)
+        reporting = NA_character_
+    )
     for (j in c("upload", "display", "evaluation", "reporting")) {
-        if (!is.null(l[[j]]))
+        if (!is.null(l[[j]])) {
             c1[[j]] <- list(l[[j]])
+        }
     }
     components <- rbind(components, c1)
 }
-save(components, file = file.path(pkg, "data", "components.rda"), compress="xz")
+save(
+    components,
+    file = file.path(pkg, "data", "components.rda"),
+    compress = "xz"
+)
 
 devtools::document(pkg)
 rcmdcheck::rcmdcheck(pkg)
