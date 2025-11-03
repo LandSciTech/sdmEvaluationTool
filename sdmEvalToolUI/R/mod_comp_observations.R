15b2652262422e8932cefd6195f62a3c0dfb8da0
Steffi LaZerte <steffi@steffi.ca>
2025-11-03 14:37:47 -0600

    Format component observation and set option for materials location for development

--- sdmEvalToolUI/R/mod_comp_observations.R
+++ sdmEvalToolUI/R/mod_comp_observations.R
@@ -1,5 +1,25 @@
 #' Title
 #'
+#' @returns
+#'
+#' @export
+#' @examples
+#' test_comp_observations()
+test_comp_observations <- function() {
+  ui <- mod_page_observations_ui()
+
+  server <- function(input, output, session) {
+    mod_page_observations_server(
+      mod = reactive("bam_v5_can71"),
+      sp = reactive("BBWO")
+    )
+  }
+
+  shiny::shinyApp(ui, server, options = list(port = 8080))
+}
+
+#' Title
+#'
 #' @param id
 #' @param title
 #'
@@ -10,6 +30,7 @@
 mod_comp_observations_ui <- function(id = "comp_observations") {
   tagList(
     div(
+      style = "position: relative;",
       leaflet::leafletOutput(NS(id, "observations")),
       absolutePanel(uiOutput(NS(id, "ui_selectors")), top = 10, right = 10)
     )
@@ -27,29 +48,70 @@ mod_comp_observations_server <- function(id = "comp_observations", sp, mod) {
 
     # TODO: Options for when materials don't exist
 
-    obs <- reactive({
-      req(sp(), mod())
-      sdmEvalToolCore:::make_target_path(
-        paste0("materials/{mod}/species/{sp}/observations.gpkg"),
-        data = list(mod = mod(), sp = sp())
-      ) |>
-        sdmEvalToolCore:::read_file() |>
-        dplyr::mutate(year = lubridate::year(time))
-    })
+    obs <- reactive(obs_prep(mod(), sp()))
 
     output$ui_selectors <- renderUI({
       tagList(
-        selectInput("year", label = "Year", choices = unique(obs()$year)),
-        selectInput("method", label = "Method", choices = unique(obs()$method))
+        selectInput("year", label = "Year", choices = sort(unique(obs()$year))),
+        selectInput(
+          "method",
+          label = "Method",
+          choices = sort(unique(obs()$method))
+        )
       )
     })
 
-    output$observations <- leaflet::renderLeaflet({
-      obs() |>
-        sf::st_transform(crs = 4326) |>
-        leaflet::leaflet() |>
-        leaflet::addTiles() |>
-        leaflet::addCircles()
-    })
+    output$observations <- leaflet::renderLeaflet(obs_map(obs()))
   })
+}
+
+
+#' Title
+#'
+#' @param obs
+#'
+#' @returns
+#'
+#' @export
+#' @examples
+#' obs_prep(mod = "bam_v5_can71", sp = "BBWO") |>
+#'   obs_map()
+
+obs_map <- function(obs) {
+  pal <- leaflet::colorFactor("darkgreen", obs$detections)
+  obs |>
+    dplyr::filter(!is.na(detections)) |>
+    sf::st_transform(crs = 4326) |>
+    leaflet::leaflet() |>
+    leaflet::addTiles() |>
+    leaflet::addCircleMarkers(color = ~ pal(detections), popup = ~popup)
+}
+
+#' Title
+#'
+#' @param obs
+#'
+#' @returns
+#'
+#' @export
+#' @examples
+#' obs_prep(mod = "bam_v5_can71", sp = "BBWO")
+
+obs_prep <- function(mod, sp) {
+  req(sp, mod)
+  sdmEvalToolCore:::make_target_path(
+    paste0("materials/{mod}/species/{sp}/observations.gpkg"),
+    data = list(mod = mod, sp = sp)
+  ) |>
+    sdmEvalToolCore:::read_file() |>
+    dplyr::mutate(
+      year = lubridate::year(.data$time),
+      detections = dplyr::na_if(.data$status > 0, 0),
+      # fmt: skip
+      popup = paste0(
+          "<strong>Method:</strong> ", .data$method, "<br>",
+          "<strong>Time:</strong> ", .data$time, "<br>",
+          "<strong>Status:</strong> ", .data$status
+        )
+    )
 }
---
+++ sdmEvalToolUI/R/zzz.R
@@ -0,0 +1,4 @@
+.onLoad <- function(libname, pkgname) {
+  # TODO: Change this, just for development right now
+  sdmEvalToolCore::sdmevaltool_options(base = "../misc/base")
+}
