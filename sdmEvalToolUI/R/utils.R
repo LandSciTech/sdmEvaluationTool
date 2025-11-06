fc4b7f1f6874df27e4c4273b0f15c6b47f5f9203
Steffi LaZerte <steffi@steffi.ca>
2025-11-06 14:05:15 -0600

    import sdmEvalToolCore and use those DB functions

--- sdmEvalToolUI/R/utils.R
+++ sdmEvalToolUI/R/utils.R
@@ -1,25 +1,10 @@
 prep_data <- function(lang = "english") {
   # TODO: Assign this elsewhere?
-  sdmEvalToolCore::sdmevaltool_options(base = "../misc/base")
+  sdmevaltool_options(base = "../misc/base")
 
-  path_data <- file.path(
-    sdmEvalToolCore::sdmevaltool_options()$base,
-    "sdm_evaluation_db.sqlite"
-  )
-
-  db <- DBI::dbConnect(path_data, drv = RSQLite::SQLite())
-  tbl_models <- dplyr::tbl(db, "models") |>
-    dplyr::collect()
-  tbl_species <- dplyr::tbl(db, "species") |>
-    dplyr::collect() |>
-    dplyr::mutate(
-      species_display = paste0(
-        .data[[paste0(lang, "_name")]],
-        " (",
-        .data$scientific_name,
-        ")"
-      )
-    )
+  db <- db_connect()
+  tbl_models <- db_read_models(db)
+  tbl_species <- db_read_species(db)
   tbl_materials <- dplyr::tbl(db, "materials") |>
     dplyr::collect()
 
