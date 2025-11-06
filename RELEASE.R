a1a5dcd95bb45a226c5e91549ee6fe4b88e14c4e
Peter Solymos <psolymos@gmail.com>
2025-11-05 20:33:16 -0700

    Add install from gh
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- RELEASE.R
+++ RELEASE.R
@@ -107,6 +107,7 @@ save(
 devtools::document(pkg)
 rcmdcheck::rcmdcheck(pkg)
 # devtools::install(pkg, upgrade = "never")
+# remotes::install_github("LandSciTech/sdmEvaluationTool/sdmEvalToolCore")
 
 devtools::load_all(pkg)
 
@@ -121,5 +122,6 @@ pkg <- "sdmEvalToolUI"
 devtools::document(pkg)
 rcmdcheck::rcmdcheck(pkg)
 # devtools::install(pkg, upgrade = "never")
+# remotes::install_github("LandSciTech/sdmEvaluationTool/sdmEvalToolUI")
 
 devtools::load_all(pkg)
