1c8c2261a61e09804f9460bea709adff51ff3a8a
Steffi LaZerte <steffi@steffi.ca>
2025-11-14 13:02:36 -0600

    Create overall testing page module

---
+++ sdmEvalToolUI/R/mod_page_test.R
@@ -0,0 +1,132 @@
+#' Test the 'test' page
+#'
+#' @returns
+#' @noRd
+#'
+#' @examplesIf have_data()
+#' test_page_test()
+#' test_page_test(NULL, NULL, NULL)
+
+test_page_test <- function(
+  deployment_id = "deployment1",
+  model_id = "bam_v5_can71",
+  species_id = "BBWO"
+) {
+  # TODO: define location, pages, etc. elsewhere
+  prep_data() |> expand_list()
+
+  ui <- bslib::page_navbar(
+    title = "SDM Tool Testing",
+    mod_page_test_ui()
+  )
+
+  server <- function(input, output, session) {
+    mod_page_test_server(
+      deployment_id = reactive(deployment_id),
+      model_id = reactive(model_id),
+      species_id = reactive(species_id),
+      tbl_deployments = tbl_deployments,
+      tbl_models = tbl_models,
+      tbl_species = tbl_species
+    )
+  }
+
+  shiny::shinyApp(ui, server, options = list(port = 8080))
+}
+
+#' Title
+#'
+#' @param id
+#' @param title
+#'
+#' @returns
+#'
+#' @export
+#' @examples
+mod_page_test_ui <- function(
+  id = "test",
+  title = "test",
+  review_width = "50%"
+) {
+  nav_panel(
+    title,
+    layout_sidebar(
+      sidebar = sidebar(
+        width = review_width,
+        position = "right",
+        "Evaluations",
+        uiOutput(NS(id, "ui_questions"))
+      ),
+      h2(textOutput(NS(id, "title"))),
+      textOutput(NS(id, "details")),
+      tableOutput(NS(id, "saved"))
+    )
+  )
+}
+
+#' Title
+#'
+#' @param id
+#'
+#' @returns
+#'
+#' @export
+#' @examples
+mod_page_test_server <- function(id = "test", ...) {
+  expand_dots(...)
+  stopifnot(is.reactive(deployment_id))
+  stopifnot(is.reactive(model_id))
+  stopifnot(is.reactive(species_id))
+
+  moduleServer(id, function(input, output, session) {
+    output$title <- renderText("Test")
+    output$details <- renderText({
+      paste0(
+        "Test for Model: ",
+        model_id(),
+        " and species ",
+        species_id()
+      )
+    })
+
+    # Evaluations ----------------------------------------------------
+    questions_init <- reactive({
+      #TODO: Read values from disk?
+      prep_questions(
+        component_id = "test",
+        deployment_id = deployment_id(),
+        model_id = model_id(),
+        species_id = species_id()
+      )
+    }) |>
+      bindCache(deployment_id(), model_id(), species_id())
+
+    output$ui_questions <- renderUI({
+      ui_questions(questions_init(), session)
+    })
+
+    # TODO: Save values temporarily, so not lost if don't "Save Responses"?
+    #   Highlight Save Responses in different colours if not saved
+    #   Highlight Page tab in different colours if not saved
+    #   Modal warns user when switching deployments/models/species if have unsaved work.
+
+    questions <- reactive({
+      #TODO: capture values as...JSON?
+      #TODO: Save values to disk
+      #TODO: Warn user if overwriting?
+      dplyr::mutate(
+        questions_init(),
+        evaluations = purrr::map(.data$id, \(q) {
+          # Grab the value; if NULL use ""
+          rlang::set_names(input[[q]] %||% "", q)
+        })
+      )
+    }) |>
+      bindEvent(input$save)
+
+    output$saved <- renderTable({
+      s <- unlist(questions()$evaluations)
+      data.frame(question = names(s), value = unname(s))
+    })
+  })
+}
