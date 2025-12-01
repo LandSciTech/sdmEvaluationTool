707779f44e6d1d78ddc4a50f91b92ef0394389f4
Peter Solymos <psolymos@gmail.com>
2025-12-01 02:09:45 -0700

    Add comp function for subunits
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

---
+++ sdmEvalToolUI/R/subunits.R
@@ -0,0 +1,18 @@
+# Subunits are used by multiple components for a deployment
+
+#' Prepare Deployment Subunits
+#'
+#' @param deployment_id Character. Deployment ID
+#'
+#' @returns Subunits polygons
+#'
+#' @export
+#' @examplesIf have_data()
+#' deployment_subunits_prep(deployment_id = "deployment1")
+
+deployment_subunits_prep <- function(deployment_id) {
+    prep_deployments(
+        deployment_id = deployment_id,
+        deployment_type = "deployment_subunits"
+    )
+}
