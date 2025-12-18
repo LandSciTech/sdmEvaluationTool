3733926611e859a68ba81e2389b655dca5e7d741
Peter Solymos <psolymos@gmail.com>
2025-12-18 17:41:15 +0100

    Add grid subunits and spp table
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- working/subunits.R
+++ working/subunits.R
@@ -5,7 +5,7 @@ x <- sf::st_read("misc/base/deployments/deployment1/deployment_subunits.gpkg")
 x1 <- st_make_grid(x)
 x2 <- st_make_grid(x, square = FALSE)
 
-df <- st_sf(subunit_id = 1:length(x1), geometry = x1)
+df <- st_sf(subunit_id = as.character(1:length(x1)), geometry = x1)
 df <- st_transform(df, 4326)
 
 st_write(
