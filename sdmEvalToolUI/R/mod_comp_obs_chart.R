b1217d13a10558421e641a57b46b0be636b16ac3
Peter Solymos <psolymos@gmail.com>
2026-01-28 23:23:44 -0700

    Add hours to obs chart
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- sdmEvalToolUI/R/mod_comp_obs_chart.R
+++ sdmEvalToolUI/R/mod_comp_obs_chart.R
@@ -9,7 +9,7 @@
 #' test_comp_obs_chart()
 
 test_comp_obs_chart <- function(...) {
-  test_comp("mod_comp_obs_chart", use = c("model_id", "species_id"), ...)
+    test_comp("mod_comp_obs_chart", use = c("model_id", "species_id"), ...)
 }
 
 #' Observations Chart Component UI
@@ -24,47 +24,49 @@ test_comp_obs_chart <- function(...) {
 #' mod_comp_obs_chart_ui()
 
 mod_comp_obs_chart_ui <- function(id = "comp_obs_chart", header = NULL) {
-  sdm_card(
-    header,
-    div(
-      style = "display:grid; grid-template-columns: 200px 200px 200px; gap: 10px; padding-bottom:10px;",
-      # style = "display:inline-block",
-      selectInput(
-        NS(id, "summary"),
-        label = "Summarize",
-        choices = c(
-          "Observations" = "nobs",
-          "Detections" = "ndet"
+    sdm_card(
+        header,
+        div(
+            style = "display:grid; grid-template-columns: 200px 200px 200px; gap: 10px; padding-bottom:10px;",
+            # style = "display:inline-block",
+            selectInput(
+                NS(id, "summary"),
+                label = "Summarize",
+                choices = c(
+                    "Observations" = "nobs",
+                    "Detections" = "ndet"
+                )
+            ),
+            selectInput(
+                NS(id, "groups"),
+                label = "Group by",
+                choices = c(
+                    "Year" = "year",
+                    "Month" = "month",
+                    "Hours (after sunrise)" = "hours",
+                    "Method" = "method"
+                )
+            ),
+            selectInput(
+                NS(id, "fill"),
+                label = "Color by",
+                choices = c(
+                    "None" = "none",
+                    # "Year" = "year",
+                    "Month" = "month",
+                    "Hours (after sunrise)" = "hours",
+                    "Method" = "method"
+                )
+            )
+        ),
+        # Chart output
+        layout_column_wrap(
+            width = 1 / 2,
+            height = 400,
+            plotly::plotlyOutput(NS(id, "obs_chart_counts")),
+            plotly::plotlyOutput(NS(id, "obs_chart_groups"))
         )
-      ),
-      selectInput(
-        NS(id, "groups"),
-        label = "Group by",
-        choices = c(
-          "Year" = "year",
-          "Month" = "month",
-          "Method" = "method"
-        )
-      ),
-      selectInput(
-        NS(id, "fill"),
-        label = "Color by",
-        choices = c(
-          "None" = "none",
-          # "Year" = "year",
-          "Month" = "month",
-          "Method" = "method"
-        )
-      )
-    ),
-    # Chart output
-    layout_column_wrap(
-      width = 1 / 2,
-      height = 400,
-      plotly::plotlyOutput(NS(id, "obs_chart_counts")),
-      plotly::plotlyOutput(NS(id, "obs_chart_groups"))
     )
-  )
 }
 
 
@@ -78,109 +80,115 @@ mod_comp_obs_chart_ui <- function(id = "comp_obs_chart", header = NULL) {
 #'
 #' @export
 mod_comp_obs_chart_server <- function(
-  id = "comp_obs_chart",
-  model_id,
-  species_id
+    id = "comp_obs_chart",
+    model_id,
+    species_id
 ) {
-  stopifnot(is.reactive(model_id))
-  stopifnot(is.reactive(species_id))
+    stopifnot(is.reactive(model_id))
+    stopifnot(is.reactive(species_id))
 
-  moduleServer(id, function(input, output, session) {
-    # Observations -------------------------------------------
-    obs <- reactive({
-      obs <- sf::st_drop_geometry(obs_prep(model_id(), species_id()))
-      dt <- as.POSIXlt(obs$time)
-      obs$year <- dt$year + 1900
-      obs$month <- factor(month.abb[dt$mo + 1], month.abb)
-      obs
+    moduleServer(id, function(input, output, session) {
+        # Observations -------------------------------------------
+        obs <- reactive({
+            obs <- sf::st_drop_geometry(obs_prep(model_id(), species_id()))
+            dt <- as.POSIXlt(obs$time)
+            obs$year <- dt$year + 1900
+            obs$month <- factor(month.abb[dt$mo + 1], month.abb)
+            obs$hours <- round(obs$hssr)
+            obs
+        })
+
+        # Chart -------------------------------------------
+        output$obs_chart_counts <- plotly::renderPlotly({
+            oo <- obs()
+            if (input$summary == "ndet") {
+                oo <- oo |> dplyr::filter(.data$status > 0)
+            }
+            if (input$fill == "none") {
+                p_counts <- oo |>
+                    ggplot2::ggplot(ggplot2::aes(x = .data$status))
+            } else {
+                p_counts <- oo |>
+                    ggplot2::ggplot(ggplot2::aes(
+                        x = .data$status,
+                        group = .data[[input$fill]],
+                        fill = as.factor(.data[[input$fill]])
+                    )) +
+                    ggplot2::labs(
+                        fill = tools::toTitleCase(input$fill)
+                    )
+            }
+            p_counts <- p_counts +
+                ggplot2::geom_bar() +
+                ggplot2::theme_light() +
+                ggplot2::xlab("Counts") +
+                ggplot2::ylab("Frequency") # +
+            # ggplot2::scale_y_log10()
+
+            plotly::ggplotly(p_counts)
+        })
+
+        output$obs_chart_groups <- plotly::renderPlotly({
+            if (input$fill == "none") {
+                det <- obs() |>
+                    dplyr::mutate(det = ifelse(status > 0, 1, 0)) |>
+                    dplyr::group_by(.data[[input$groups]]) |>
+                    dplyr::summarize(
+                        nobs = dplyr::n(),
+                        ndet = sum(det),
+                        .groups = "keep"
+                    )
+                p_groups <- det |>
+                    ggplot2::ggplot(ggplot2::aes(
+                        x = .data[[input$groups]],
+                        y = .data[[input$summary]]
+                    ))
+            } else {
+                if (input$groups == input$fill) {
+                    det <- obs() |>
+                        dplyr::mutate(det = ifelse(status > 0, 1, 0)) |>
+                        dplyr::group_by(
+                            .data[[input$groups]]
+                        ) |>
+                        dplyr::summarize(
+                            nobs = dplyr::n(),
+                            ndet = sum(det),
+                            .groups = "keep"
+                        )
+                    p_groups <- det |>
+                        ggplot2::ggplot(ggplot2::aes(
+                            x = .data[[input$groups]],
+                            y = .data[[input$summary]],
+                            fill = as.factor(.data[[input$groups]])
+                        )) +
+                        ggplot2::labs(fill = tools::toTitleCase(input$groups))
+                } else {
+                    det <- obs() |>
+                        dplyr::mutate(det = ifelse(status > 0, 1, 0)) |>
+                        dplyr::group_by(
+                            .data[[input$groups]],
+                            .data[[input$fill]]
+                        ) |>
+                        dplyr::summarize(
+                            nobs = dplyr::n(),
+                            ndet = sum(det),
+                            .groups = "keep"
+                        )
+                    p_groups <- det |>
+                        ggplot2::ggplot(ggplot2::aes(
+                            x = .data[[input$groups]],
+                            y = .data[[input$summary]],
+                            fill = as.factor(.data[[input$fill]])
+                        )) +
+                        ggplot2::labs(fill = tools::toTitleCase(input$fill))
+                }
+            }
+            p_groups <- p_groups +
+                ggplot2::geom_col() +
+                ggplot2::theme_light() +
+                ggplot2::xlab(tools::toTitleCase(input$fill)) +
+                ggplot2::ylab(tools::toTitleCase(input$summary))
+            plotly::ggplotly(p_groups)
+        })
     })
-
-    # Chart -------------------------------------------
-    output$obs_chart_counts <- plotly::renderPlotly({
-      if (input$fill == "none") {
-        p_counts <- obs() |>
-          ggplot2::ggplot(ggplot2::aes(x = .data$status))
-      } else {
-        p_counts <- obs() |>
-          ggplot2::ggplot(ggplot2::aes(
-            x = .data$status,
-            group = .data[[input$fill]],
-            fill = as.factor(.data[[input$fill]])
-          )) +
-          ggplot2::labs(
-            fill = tools::toTitleCase(input$fill)
-          )
-      }
-      p_counts <- p_counts +
-        ggplot2::geom_bar() +
-        ggplot2::theme_light() +
-        ggplot2::xlab("Counts") +
-        ggplot2::ylab("Frequency")
-
-      plotly::ggplotly(p_counts)
-    })
-
-    output$obs_chart_groups <- plotly::renderPlotly({
-      if (input$fill == "none") {
-        det <- obs() |>
-          dplyr::mutate(det = ifelse(status > 0, 1, 0)) |>
-          dplyr::group_by(.data[[input$groups]]) |>
-          dplyr::summarize(
-            nobs = dplyr::n(),
-            ndet = sum(det),
-            .groups = "keep"
-          )
-        p_groups <- det |>
-          ggplot2::ggplot(ggplot2::aes(
-            x = .data[[input$groups]],
-            y = .data[[input$summary]]
-          ))
-      } else {
-        if (input$groups == input$fill) {
-          det <- obs() |>
-            dplyr::mutate(det = ifelse(status > 0, 1, 0)) |>
-            dplyr::group_by(
-              .data[[input$groups]]
-            ) |>
-            dplyr::summarize(
-              nobs = dplyr::n(),
-              ndet = sum(det),
-              .groups = "keep"
-            )
-          p_groups <- det |>
-            ggplot2::ggplot(ggplot2::aes(
-              x = .data[[input$groups]],
-              y = .data[[input$summary]],
-              fill = as.factor(.data[[input$groups]])
-            )) +
-            ggplot2::labs(fill = tools::toTitleCase(input$groups))
-        } else {
-          det <- obs() |>
-            dplyr::mutate(det = ifelse(status > 0, 1, 0)) |>
-            dplyr::group_by(
-              .data[[input$groups]],
-              .data[[input$fill]]
-            ) |>
-            dplyr::summarize(
-              nobs = dplyr::n(),
-              ndet = sum(det),
-              .groups = "keep"
-            )
-          p_groups <- det |>
-            ggplot2::ggplot(ggplot2::aes(
-              x = .data[[input$groups]],
-              y = .data[[input$summary]],
-              fill = as.factor(.data[[input$fill]])
-            )) +
-            ggplot2::labs(fill = tools::toTitleCase(input$fill))
-        }
-      }
-      p_groups <- p_groups +
-        ggplot2::geom_col() +
-        ggplot2::theme_light() +
-        ggplot2::xlab(tools::toTitleCase(input$fill)) +
-        ggplot2::ylab(tools::toTitleCase(input$summary))
-      plotly::ggplotly(p_groups)
-    })
-  })
 }
