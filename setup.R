c0536154a3702b29a586282db7ec5fd62185de80
Peter Solymos <psolymos@gmail.com>
2026-02-12 20:23:28 -0700

    Setup script
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

---
+++ setup.R
@@ -0,0 +1,25 @@
+message("=========================================")
+message("   Installing the SDM Evaluation Tool")
+message("=========================================")
+message("------ Downloading results --------------")
+download.file(
+    "https://peter.solymos.org/testapi/sdmevaltool/sdm_evaluation_results.zip",
+    "./sdm_evaluation_results.zip",
+    method = "libcurl"
+)
+message("------ Unzipping contents ---------------")
+unzip("./sdm_evaluation_results.zip")
+message("------ Installing R packages ------------")
+if (!requireNamespace("remotes")) {
+    install.packages("remotes")
+}
+remotes::install_github(
+    "LandSciTech/sdmEvaluationTool/sdmEvalToolCore",
+    dependencies = TRUE
+)
+remotes::install_github(
+    "LandSciTech/sdmEvaluationTool/sdmEvalToolUI",
+    dependencies = TRUE
+)
+message("------ Done! ----------------------------")
+message("Try running: `sdmEvalToolUI::sdm_tool()`")
