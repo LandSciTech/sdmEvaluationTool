77519051fd0b0ddcbf3f62e389c078fd17eec5f4
Hughes <Josie.Hughes@ec.gc.ca>
2026-04-27 17:58:28 -0400

    Avoiding google drive permission problems by manually downloading the sdm_evaluation_results.

--- setup_JH.R
+++ setup_JH.R
@@ -4,22 +4,22 @@
 message("=========================================")
 message("   Installing the SDM Evaluation Tool")
 message("=========================================")
-message("------ Downloading results --------------")
-if (!requireNamespace("googledrive")) {
-  install.packages("googledrive")
-}
+#message("------ Downloading results --------------")
+#googledrive::drive_download(
+#  "https://drive.google.com/file/d/12dZ8vpiNuusICc4b1QyREr1NI8HQAM1t/view?usp=drive_link",
+#  path = "./sdm_evaluation_results.zip",
+#  overwrite=T
+#)
+
 if(file.exists("./sdm_evaluation_results")){
   if(file.exists("./sdm_evaluation_results_old")){
     stop("You already have old results in your sdm_evaluation_results_old folder.
          To proceed, delete or remove sdm_evaluation_results_old then rerun this script.")
   }
+  message("-- Moving old results to sdm_evaluation_results_old --")
   file.rename("./sdm_evaluation_results","./sdm_evaluation_results_old")
 }
-googledrive::drive_download(
-  "https://drive.google.com/file/d/12dZ8vpiNuusICc4b1QyREr1NI8HQAM1t/view?usp=drive_link",
-  path = "./sdm_evaluation_results.zip",
-  overwrite=T
-)
+
 message("------ Unzipping contents ---------------")
 unzip("./sdm_evaluation_results.zip")
 message("------ Installing R packages ------------")
