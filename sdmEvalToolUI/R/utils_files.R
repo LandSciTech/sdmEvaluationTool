f106efdb061784e1bfc4fa513cdafca893bd4f82
Hughes <Josie.Hughes@ec.gc.ca>
2026-07-21 10:02:35 -0400

    Fix for https://github.com/LandSciTech/sdmEvaluationTool/issues/87

--- sdmEvalToolUI/R/utils_files.R
+++ sdmEvalToolUI/R/utils_files.R
@@ -360,9 +360,11 @@ fetch_questions <- function(deployment_id, component_id) {
   if (any(component_id != "ALL")) {
     q <- dplyr::filter(q, .data$component %in% .env$component_id)
   }
-  # TODO: Temporary until all questions updated
-  if (!"metadata_id" %in% names(q)) {
-    q$metadata_id <- NA_character_
+  if(nrow(q)>0){
+    # TODO: Temporary until all questions updated
+    if (!"metadata_id" %in% names(q)) {
+      q$metadata_id <- NA_character_
+    }
   }
 
   q
