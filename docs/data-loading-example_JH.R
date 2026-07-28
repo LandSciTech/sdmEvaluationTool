338d4e8f745d7f0cd2d8c1f636e5522ff838e514
Carina Firkowski <114943759+Carina-Firkowski@users.noreply.github.com>
2026-07-28 10:51:58 -0400

    Update data-loading-example_JH.R

--- docs/data-loading-example_JH.R
+++ docs/data-loading-example_JH.R
@@ -10,8 +10,8 @@ library(suntools)
 library(sdmEvalToolCore)
 library(sdmEvalToolUI)
 #optionally use local version of the package
-if (requireNamespace("devtools", quietly = TRUE)) devtools::load_all("sdmEvalToolCore")
-if (requireNamespace("devtools", quietly = TRUE)) devtools::load_all("sdmEvalToolUI")
+#if (requireNamespace("devtools", quietly = TRUE)) devtools::load_all("sdmEvalToolCore")
+#if (requireNamespace("devtools", quietly = TRUE)) devtools::load_all("sdmEvalToolUI")
 
 path <- "./BAM Demo"
 #path <- "C:/Users/HughesJo/Documents/InitialWork/NOBMWG/MET (Model Evaluation Tool) Google Drive Copy/Materials/Model Upload/BAM Demo"
@@ -158,6 +158,8 @@ fi <- file.path(path, "predictors", "predictor_metadata.csv")
 x <- read_file(fi)
 names(x)[names(x)=="variable"]="predictor"
 x <- x[, rule$output$columns]
+
+user_id <- "testuser"
 
 prep_predictor_metadata(
   x = x,
@@ -412,6 +414,7 @@ prep_deployment_subunits(
   )
 #}
 
+# End previous function to run the following:
 #if (FALSE) {
   # #zip deployment materials for sharing.
   # file.copy(
